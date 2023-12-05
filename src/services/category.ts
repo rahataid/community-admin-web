22b2496aa7e5797dc9351175040fc8e71bd2bafd
RS21 <rs21@pop-os.localdomain>
2023-12-05 15:03:33 +0545

    bar graph updated

--- src/services/category.ts
+++ src/services/category.ts
@@ -3,6 +3,7 @@ import { ICategoryApiFilter, ICategoryDetails } from 'src/types/community';
 
 const CategoryService = {
   list: (params?: ICategoryApiFilter) => axiosInstance.get(endpoints.category.list, { params }),
+  countCommunity: () => axiosInstance.get(endpoints.category.countCommunity),
   create: (data: ICategoryDetails) => axiosInstance.post(endpoints.category.create, data),
   edit: (id: string, data: any) => axiosInstance.patch(endpoints.category.edit(id), data),
 };
